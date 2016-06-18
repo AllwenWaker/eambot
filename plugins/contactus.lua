f3329a20fccaf81d4ebca544cd921460cfde25f7
allwen <ArianGhasempour40@Gmail.com>
2016-06-18 14:19:54 +0430

    Update contactus.lua

--- plugins/contactus.lua
+++ plugins/contactus.lua
@@ -17,7 +17,7 @@ if chat_info == 'block' then
  api.sendMessage(msg.chat.id, '`- Sorry !`\n_You are Blocked_', true) 
 else
  db:hset(hash, user_id, 'true')
- api.sendMessage(msg.chat.id, '*- Chat Started*\n `Now all messages will be send to BeatBotTeam Members`\n\n_please send your message now!_', true) 
+ api.sendMessage(msg.chat.id, '*- Chat Started*\n `Now all messages will be send to Allwen`\n\n_please send your message now!_', true) 
  end
  end
 if blocks[1] == 'end' then
@@ -30,18 +30,18 @@ end
 end
 if msg.chat.type == 'private' and chat_info == 'true' then
 if blocks[1] == 'end' or blocks[1] == 'chat' then return nil end
-api.forwardMessage('-119919729', msg.chat.id, msg_id) 
+api.forwardMessage('-104797667', msg.chat.id, msg_id) 
 api.sendKeyboard(msg.chat.id, '`- Sent !`\n_Please wait for reply_'  ,do_keyboard_endchat(), true)
 end
 if blocks[1] == 'block' then
-if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -119919729 and not blocks[2] then
+if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -104797667 and not blocks[2] then
 msg = msg.reply
 local user_id = msg.forward_from.id
  db:hset(hash, user_id, 'block')
 api.sendMessage(msg.chat.id, '_User '..user_id..' Blocked_', true) 
 api.sendMessage(user_id, '`- Sorry`\n_You are Blocked_\n\n*Chat ended.*', true) 
 else
- if msg.chat.type == 'group' and msg.chat.id == -119919729 then
+ if msg.chat.type == 'group' and msg.chat.id == -104797667 then
  if msg.reply then return nil end
 local user_id = blocks[2]
  db:hset(hash, user_id, 'block')
@@ -51,14 +51,14 @@ end
 end
 end
 if blocks[1] == 'unblock' then
-if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -119919729 and not blocks[2] then
+if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -104797667 and not blocks[2] then
 msg = msg.reply
 local user_id = msg.forward_from.id
  db:hset(hash, user_id, 'false')
 api.sendMessage(msg.chat.id, '_User '..user_id..' UnBlocked_', true) 
 api.sendMessage(user_id, '_You are UnBlocked_', true) 
 else
- if msg.chat.type == 'group' and msg.chat.id == -119919729 then
+ if msg.chat.type == 'group' and msg.chat.id == -104797667 then
   if msg.reply then return nil end
 local user_id = blocks[2]
  db:hset(hash, user_id, 'false')
@@ -67,7 +67,7 @@ api.sendMessage(user_id, '_You are UnBlocked_', true)
 end 
 end
 end
-    if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -119919729 then
+    if msg.reply and msg.reply.forward_from and msg.chat.type == 'group' and msg.chat.id == -104797667 then
    msg = msg.reply_to_message
     local receiver = msg.forward_from.id
     local input = blocks[1]
